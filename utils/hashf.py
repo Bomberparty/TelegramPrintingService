8357acc46f9aee148f097262729c862fe23ffb08
Bomberparty <ilyawersed@mail.ru>
2023-03-25 12:49:19 +0300

    Added a simple hashfunc(only 2^32 unique values)

---
+++ utils/hashf.py
@@ -0,0 +1,16 @@
+def my_hash(s):
+    # Initialize hash value to a prime number
+    hash_value = 5381
+    
+    # Loop through each character in the string
+    for c in s:
+        # Multiply hash value by a prime number
+        hash_value = (hash_value * 33) ^ ord(c)
+    
+    # Mask the hash value to 32 bits to avoid collisions
+    hash_value &= 0xFFFFFFFF
+    
+    # Ensure first 2^32 values are unique
+    hash_value += (hash_value == 0)
+    
+    return hash_value
